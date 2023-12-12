615fc5e08c397766c96e8059eb58b1e84ce9ecba
Elizabeth Danzberger <spookystuff@protonmail.com>
2023-12-12 14:31:46 -0500

    Added channel list

---
+++ src/components/ChannelList/ChannelList.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+import TextChannel from "./TextChannel.vue"
+</script>
+
+<template>
+  <div id="channelList">
+    <TextChannel name="general" />
+    <TextChannel name="memes" />
+  </div>
+</template>
+
+<style scoped>
+  #channelList {
+    background-color: #343A40;
+    display: flex;
+    flex-flow: column nowrap;
+    width: 200px;
+    font-size: 15px;
+  }
+</style>
---
+++ src/components/ChannelList/TextChannel.vue
@@ -0,0 +1,38 @@
+<script setup lang="ts">
+import { PhHash } from '@phosphor-icons/vue';
+
+interface TextChannelProps {
+  name: string,
+}
+
+const props = defineProps<TextChannelProps>();
+</script>
+
+<template>
+  <div id="channelListItem">
+    <PhHash :size="22" weight="regular" />
+    <div>{{ props.name }}</div>
+  </div>
+</template>
+
+<style scoped>
+  #channelListItem {
+    display: flex;
+    flex-flow: row nowrap;
+    align-items: center;
+    text-overflow: ellipsis;
+    margin: 2px 5px;
+    border-radius: 0.25rem;
+    color: #ADB5BD;
+  }
+
+  #channelListItem:hover {
+    background-color: #495057;
+    color: #F8F9FA;
+    cursor: pointer;
+  }
+
+  #channelListItem svg {
+    margin: 5px;
+  }
+</style>
