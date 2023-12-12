19431a1b88001932af0d8c1fda36c575191217b0
Elizabeth Danzberger <spookystuff@protonmail.com>
2023-12-12 14:31:37 -0500

    Added server list

---
+++ src/components/ServerList/ServerList.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+import ServerListIcon from './ServerListIcon.vue';
+</script>
+
+<template>
+  <div id="serverListSidebar">
+    <ServerListIcon />
+    <ServerListIcon />
+    <ServerListIcon />
+    <ServerListIcon />
+    <ServerListIcon />
+    <ServerListIcon />
+  </div>
+</template>
+
+<style scoped>
+#serverListSidebar {
+  background-color: #212529;
+  display: flex;
+  flex-flow: column nowrap;
+}
+</style>
---
+++ src/components/ServerList/ServerListIcon.vue
@@ -0,0 +1,25 @@
+<script lang="ts" setup>
+
+</script>
+
+<template>
+  <input
+      alt="Server icon"
+      src="vite.svg"
+      type="image"
+  />
+</template>
+
+<style scoped>
+#serverListSidebar input {
+  background-color: white;
+  width: 48px;
+  border-radius: 50%;
+  margin: 5px;
+  transition: border-radius 200ms;
+}
+
+#serverListSidebar input:hover {
+  border-radius: 30%;
+}
+</style>
